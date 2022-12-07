fa7ca708aa345e430a6f0b5948d4460af11e7c86
soupnwaffles <KVprivate@hotmail.com>
2022-12-06 20:55:59 -0800

    worked on first fit implicit alloc

--- memoryalloc.py
+++ memoryalloc.py
@@ -16,7 +16,7 @@ def heapstart(argv=None):
     if arg is None or len(arg)==1: 
     #If input is none, default values used.  
         print("Commandline was none, defaulted to using 5.in as an input file, with implicit free list using first fit. ")
-        arg = ["memoryalloc.py", "result", "--free-list=implicit", "--fit=first", "examples/5.in"]
+        arg = ["memoryalloc.py", "result/imfirst.txt", "--free-list=implicit", "--fit=first", "examples/5.in"]
     if (len(arg)<5): 
         printusage()
         print(arg) 
@@ -90,7 +90,7 @@ def printusage():
     print("Usage: python3 heap.py [-o <output-path>] --free-list={implicit or explicit} --fit={first or best} <input file>   ")
     return 
 
-def myalloc(heap, strategy, fit, bytes): 
+def myalloc(bytes): 
     #4 bytes per word, add 8 by default for header and footer
     #Address must be divisible by 8
     # IF allocating 5 bytes, take 5 + (8-(5%8))
@@ -99,6 +99,42 @@ def myalloc(heap, strategy, fit, bytes):
     payloadbytes= bytes + paddedbytes
     totalbytes=  8 + payloadbytes
     address= "0x{0:0{1}X}".format(totalbytes,8)
+    #First address/word is a placeholder. 
+    #Firstfit
+    #Find the first available free space when allocating. 
+    #Go through entire heap, if no space available, call mysbrk, try again
+    #If space is found, found=True
+    found = False 
+    i = 1 
+    if fit == "first" and strategy =="implicit":
+        while (found == False):  
+            decimalsize = int(heap[i], 16)
+            # If space is allocated
+            if decimalsize % 2 == 1: 
+                # Move pointer forward 
+                i += (decimalsize/4)
+                # If there is not enough space when moving to next block
+                if i >=(len(heap)-(totalbytes/4)): 
+                    mysbrk()
+            elif decimalsize % 2 == 0: 
+                # If it is free, but not enough space for allocation
+                if  decimalsize < totalbytes : 
+                    #Move forward certain amount of words to next block
+                    i += (decimalsize/4)
+                    #Increase heap if heapsize reached limit. 
+                    if i>=(len(heap)-(totalbytes/4)): 
+                        mysbrk()
+                #Otherwise, if there is enough space. 
+                elif decimalsize >= totalbytes: 
+                    found = True
+                    #Add the bytes allocated + 1 (to indicate allocation) and return pointer. 
+                    heap[i]="0x{0:0{1}X}".format(totalbytes+1,8)
+                    heap[i+(totalbytes/4)-1] = "0x{0:0{1}X}".format(totalbytes+1,8)
+                    return i
+
+
+
+            return 
     
     return 
 
@@ -109,12 +145,14 @@ def myfree(heap, address):
     # Update the header and footer of the freed block (or coalesced)
     # Update the reference/pointer in the pointerarray
     return 
+    
+  
 
 def myrealloc(heap, strategy, fit, address, bytes): 
     return 
 
-def mysbrk(heap): 
-    heap.append([0]*99000)
+def mysbrk(): 
+    heap.append([0]*9000)
 
 if __name__== "__main__": 
     heapstart(sys.argv)
